bf32a7f5a36eb7776ef37788826e6cbb1c9356d2
maicallatrace <maicallatrace@gmail.com>
2022-03-27 11:16:13 -0600

    REPO

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var  customerName = 'bob';
+
+function  upperCaseCustomerName(){
+    return customerName = 'BOB'
+}
+ function setBestCustomer(){
+     return bestCustomer = 'not bob'
+ }
+ function  overwriteBestCustomer(){
+     return bestCustomer = 'maybe bob'
+ }
+ const LeastFavoriteCustomer = 'bob'
+
+ function changeLeastFavoriteCustomer(){
+     return LeastFavoriteCustomer = 'mike'
+ }
